33bbb3565d28bbdc6579d89182ae5271a39948e8
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-05-07 23:50:30 +0530

    [Manoj]Adding_UC1

---
+++ AddressBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookAdo
+{
+    public class AddressBookRepository
+    {
+        public static string connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=AddressBookService;Trusted_connection=True;MultipleActiveResultSets=True";
+        static SqlConnection connection;
+       
+    }
+}
